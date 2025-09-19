d03548c4b2d3d80efb5d844d1d7e7c167b5c93ff
Michael Riha <michael.riha@bonrepublic.com>
2025-09-19 14:30:42 +0200

    added CI pipeline

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './app/App';
-import './app/App.css';  // This import should match the file location
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
